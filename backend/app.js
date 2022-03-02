093b7008302e5a6cb3ce0bd835ddb727638a5efc
Romain <Deloux@MacBook-Pro-de-Deloux.local>
2022-03-02 14:41:13 +0100

    ajout de dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,44 @@
+/* installing DOTENV */
 require('dotenv').config();
-console.log(process.env.MONGO_USERNAME);
 
 const express = require('express');
 const mongoose = require('mongoose');
+
+/* Path import for images */
 const path = require('path');
+
+/* Helmet import for secure HTTP headers */
 const helmet = require('helmet');
+
+/* Cors import for secure multi-origin requests */
 const cors = require('cors');
+
+/* import Rate-limit to limit requests */
 const rateLimit = require('express-rate-limit');
 
+/* import routes */
 const userRoutes = require('./routes/users');
 const sauceRoutes = require('./routes/sauces')
 
 const app = express(); 
 
-mongoose.connect('mongodb+srv://AngM:P6Angelique@cluster0.6djjc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+/* connection of the app with mongoose which manages the MongoDB database */
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.6djjc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+/* details of the rate-limit */  
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+   // 15 minutes
+    windowMs: 15 * 60 * 1000,
+    // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 100,
+    // Return rate limit info in the `RateLimit-*` headers
+    standardHeaders: true, 
+    // Disable the `X-RateLimit-*` headers
+    legacyHeaders: false, 
 });
 
 app.use(express.json());
@@ -31,6 +46,7 @@ app.use(helmet());
 app.use(cors());
 app.use(limiter);
 
+/* Prevent CORS Errors */
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -38,7 +54,10 @@ app.use((req, res, next) => {
     next();
   });
 
+/* image management */
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
+/* call of the routes */
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
 
